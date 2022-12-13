0cf7664f8e4546694a80b6c93a28a97eba70aaaf
Ian Liceranzu Ruffo <ianliceranzu@gmail.com>
2022-12-13 22:31:52 +0100

    Limpieza y arena de amistosos

--- BasketLeague2.App/Program.cs
+++ BasketLeague2.App/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7030") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ BasketLeague2.Utils/Utils/GameUtils.cs
@@ -0,0 +1,62 @@
+﻿using BasketLeague2.Utils.Models;
+
+namespace BasketLeague2.Utils.Utils
+{
+    public class GameUtils
+    {
+        public static Result SimulateGame(Team home, Team rival)
+        {
+            Random rnd = new();
+
+            bool repeat;
+
+            int hr;
+            int rr;
+
+            do
+            {
+                int ha = home.Atacar(rival, rnd);
+                int hd = home.Defender(rival, rnd);
+
+                int ra = rival.Atacar(home, rnd);
+                int rd = rival.Defender(home, rnd);
+                
+                if (ha < hd && ra < rd)
+                {
+                    hr = home.Resultado(rnd, hd, ha) / 2;
+                    rr = rival.Resultado(rnd, hd, ha) / 2;
+                }
+                else
+                {
+                    hr = home.Resultado(rnd, hd, ha);
+                    rr = rival.Resultado(rnd, rd, ra);
+                }
+
+                int diferencia = hr - rr;
+
+                if (diferencia == 0)
+                {
+                    repeat = true;
+                }
+                else if (diferencia > 0)
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    repeat = false;
+                }
+            } while (repeat);
+
+            return new Result()
+            {
+                Equipo1 = home.NombreCompleto,
+                Equipo2 = rival.NombreCompleto,
+                Fecha = DateTime.Now,
+                Resultado1 = hr,
+                Resultado2 = rr,
+            };
+        }
+
+    }
+}
