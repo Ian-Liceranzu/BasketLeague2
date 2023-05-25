2f9baf71710988cef0a322a66f6f083010b7d9ac
Ian Liceranzu Ruffo <ianliceranzu@gmail.com>
2023-05-25 15:31:18 +0200

    PlayerSystem

---
+++ BasketLeague2.Utils/Models/Player.cs
@@ -0,0 +1,62 @@
+﻿namespace BasketLeague2.Utils.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public int Team { get; set; }
+
+        public int PointsScored { get; set; }
+        public int Rebounds { get; set; }
+        public int Assists { get; set; }
+        public int Steals { get; set; }
+        public int Blocks { get; set; }
+        public int Turnovers { get; set; }
+
+        public double Rating
+        {
+            get
+            {
+                var pointsWeight = 2.0 / 7.0;
+                var reboundsWeight = 1.0 / 7.0;
+                var assistsWeight = 1.0 / 7.0;
+                var stealsWeight = 2.0 / 7.0;
+                var blocksWeight = 2.0 / 7.0;
+                var turnoversWeight = -1.0 / 7.0;
+
+                return PointsScored * pointsWeight +
+                       Rebounds * reboundsWeight +
+                       Assists * assistsWeight +
+                       Steals * stealsWeight +
+                       Blocks * blocksWeight +
+                       Turnovers * turnoversWeight;
+            }
+            set { }
+        }
+    }
+
+    public class RandomNameGenerator
+    {
+        private static readonly string[] _firstNames = { "Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Isabella", "Jack", "Kate", "Liam", "Mia", "Noah", "Olivia", "Penelope", "Quinn", "Ruby", "Sophia", "Thomas", "Ursula", "Victoria", "William", "Xander", "Yara", "Zoe" };
+        private static readonly string[] _lastNames = { "Adams", "Baker", "Carter", "Davis", "Edwards", "Fisher", "Garcia", "Hernandez", "Ingram", "Johnson", "Khan", "Lee", "Miller", "Nguyen", "O'Brien", "Patel", "Quinn", "Rodriguez", "Smith", "Taylor", "Upton", "Vargas", "Wilson", "Xu", "Yang", "Zhang" };
+        private static readonly string[] _nicknames = { "Ace", "Buddy", "Champ", "Duke", "Foxy", "Guru", "Hawk", "Jazz", "Knight", "Lucky", "Maverick", "Ninja", "Panda", "Queen", "Ranger", "Shark", "Tiger", "Viper", "Wizard", "X-Man", "Yogi", "Zeus" };
+
+        public static string GenerateName()
+        {
+            Random random = new();
+            int firstNameIndex = random.Next(_firstNames.Length);
+            int lastNameIndex = random.Next(_lastNames.Length);
+            string firstName = _firstNames[firstNameIndex];
+            string lastName = _lastNames[lastNameIndex];
+            return firstName + " " + lastName;
+        }
+
+        public static string GenerateNickname()
+        {
+            Random random = new();
+            int nicknameIndex = random.Next(_nicknames.Length);
+            string nickname = _nicknames[nicknameIndex];
+            return nickname;
+        }
+    }
+}
--- BasketLeague2.Utils/Utils/GameUtils.cs
+++ BasketLeague2.Utils/Utils/GameUtils.cs
@@ -58,5 +58,55 @@ namespace BasketLeague2.Utils.Utils
             };
         }
 
+        public static void SimulateGameWithPlayers(List<Player> homePlayers, List<Player> rivalPlayers)
+        {
+            // Define the weights for each stat
+            int[] weights = { 2, 1, 1, 2, 2, -1 };
+
+            // Generate random stats for each player on each team
+            var rand = new Random();
+            var teamAStats = GenerateStats(homePlayers, rand);
+            var teamBStats = GenerateStats(rivalPlayers, rand);
+
+            // Calculate the total score for each team
+            var teamAScore = CalculateScore(teamAStats, weights);
+            var teamBScore = CalculateScore(teamBStats, weights);
+
+            // Determine the winner and print the result
+            var winner = (teamAScore > teamBScore) ? "Team A" : "Team B";
+            Console.WriteLine("The winner is {0} with a score of {1}-{2}", winner, teamAScore, teamBScore);
+        }
+
+        public static int[][] GenerateStats(List<Player> players, Random rand)
+        {
+            var stats = new int[players.Count][];
+            for (var i = 0; i < players.Count; i++)
+            {
+                stats[i] = new int[6];
+                stats[i][0] =  (int)(rand.Next(0, 30) * (players[i].PointsScored / 10.0));  // Points scored
+                stats[i][1] = (int)(rand.Next(0, 15) * (players[i].Rebounds / 10.0));  // Rebounds
+                stats[i][2] = (int)(rand.Next(0, 10) * (players[i].Assists / 10.0));  // Assists
+                stats[i][3] = (int)(rand.Next(0, 5) * (players[i].Steals / 10.0));   // Steals
+                stats[i][4] = (int)(rand.Next(0, 5) * (players[i].Blocks / 10.0));   // Blocks
+                stats[i][5] = (int)(rand.Next(0, 5) * (players[i].Turnovers / 10.0));   // Turnovers
+            }
+            return stats;
+        }
+
+        private static int CalculateScore(int[][] stats, int[] weights)
+        {
+            var totalScore = 0;
+            for (var i = 0; i < stats.Length; i++)
+            {
+                var playerScore = 0;
+                for (var j = 0; j < stats[i].Length; j++)
+                {
+                    playerScore += stats[i][j] * weights[j];
+                }
+                totalScore += playerScore;
+            }
+            return totalScore;
+        }
+
     }
 }
