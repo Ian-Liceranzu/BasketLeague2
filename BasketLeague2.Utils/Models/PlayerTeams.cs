cafbb694bd6e8dc32cbc03030b0adf61e2e900e2
Ian <43787552+Ian-Inizias@users.noreply.github.com>
2023-09-15 09:45:26 +0200

    All player menus

---
+++ BasketLeague2.Utils/Models/PlayerTeams.cs
@@ -0,0 +1,9 @@
+﻿namespace BasketLeague2.Utils.Models;
+
+public class PlayerTeams
+{
+    public int Player { get; set; }
+    public int Team { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime? End { get; set; }
+}
