a0ea8731c0d1312086d170809b99cd356992f855
Ian <43787552+Ian-Inizias@users.noreply.github.com>
2024-02-13 09:57:16 +0100

    General reworks + PO2

--- BasketLeague2.Utils/Models/Player.cs
+++ BasketLeague2.Utils/Models/Player.cs
@@ -1,111 +1,110 @@
-﻿namespace BasketLeague2.Utils.Models
+﻿namespace BasketLeague2.Utils.Models;
+
+public class Player
 {
-    public class Player
+    public int Codigo { get; set; }
+    public string Name { get; set; } = "";
+    public string Nickname { get; set; } = "";
+    public int Team { get; set; }
+
+    public int InsideScoring { get; set; }
+    public int OutsideScoring { get; set; }
+    public int Athleticism { get; set; }
+    public int Playmaking { get; set; }
+    public int Rebounding { get; set; }
+    public int Defending { get; set; }
+    public double Overall { get; set; }
+
+    public Player()
     {
-        public int Codigo { get; set; }
-        public string Name { get; set; }
-        public string Nickname { get; set; }
-        public int Team { get; set; }
-
-        public int InsideScoring { get; set; }
-        public int OutsideScoring { get; set; }
-        public int Athleticism { get; set; }
-        public int Playmaking { get; set; }
-        public int Rebounding { get; set; }
-        public int Defending { get; set; }
-        public double Overall { get; set; }
-
-        public Player()
-        {
-        }
-
-        public Player(string name, string nickname, int team, int insideScoring, int outsideScoring, int athleticism,
-            int playmaking, int rebounding, int defending)
-        {
-            Name = name;
-            Nickname = nickname;
-            Team = team;
-            InsideScoring = insideScoring;
-            OutsideScoring = outsideScoring;
-            Athleticism = athleticism;
-            Playmaking = playmaking;
-            Rebounding = rebounding;
-            Defending = defending;
-            Overall = CalculateOverall();
-        }
-
-        public Player(int insideScoring, int outsideScoring, int athleticism, int playmaking, int rebounding,
-            int defending)
-        {
-            Name = RandomNameGenerator.GenerateName();
-            Nickname = RandomNameGenerator.GenerateNickname();
-            InsideScoring = insideScoring;
-            OutsideScoring = outsideScoring;
-            Athleticism = athleticism;
-            Playmaking = playmaking;
-            Rebounding = rebounding;
-            Defending = defending;
-            Overall = CalculateOverall();
-        }
-
-        public static List<Player> RandomPlayer(int num)
-        {
-            Random random = new();
-            List<Player> players = new();
-            for (var i = 0; i < num; i++)
-            {
-                players.Add(new Player(random.Next(70, 80), random.Next(70, 80), random.Next(70, 80),
-                    random.Next(70, 80), random.Next(70, 80), random.Next(70, 80)));
-            }
-
-            return players;
-        }
-
-        private double CalculateOverall()
-        {
-            return (InsideScoring + OutsideScoring + Athleticism + Playmaking + Rebounding + Defending) / 6.0;
-        }
     }
 
-    public static class RandomNameGenerator
+    public Player(string name, string nickname, int team, int insideScoring, int outsideScoring, int athleticism,
+        int playmaking, int rebounding, int defending)
     {
-        private static readonly string[] FirstNames =
-        {
-            "Aiden", "Amelia", "Aria", "Ava", "Caden", "Charlotte", "David", "Elijah", "Emily", "Grace", "Grayson",
-            "Harper", "Isabella", "Jackson", "John", "Landon", "Liam", "Lily", "Lucas", "Mateo", "Mia", "Nicole",
-            "Noah", "Olivia", "Sophia", "Sophie", "Zoe"
-        };
+        Name = name;
+        Nickname = nickname;
+        Team = team;
+        InsideScoring = insideScoring;
+        OutsideScoring = outsideScoring;
+        Athleticism = athleticism;
+        Playmaking = playmaking;
+        Rebounding = rebounding;
+        Defending = defending;
+        Overall = CalculateOverall();
+    }
 
-        private static readonly string[] LastNames =
-        {
-            "Allen", "Anderson", "Brown", "Clark", "Davis", "Garcia", "Hall", "Harris", "Jackson", "Johnson", "Jones",
-            "King", "Lee", "Lewis", "Martinez", "Martin", "Miller", "Moore", "Robinson", "Rodriguez", "Smith", "Taylor",
-            "Thomas", "Thompson", "Walker", "White", "Williams", "Wilson", "Wright", "Young"
-        };
+    public Player(int insideScoring, int outsideScoring, int athleticism, int playmaking, int rebounding,
+        int defending)
+    {
+        Name = RandomNameGenerator.GenerateName();
+        Nickname = RandomNameGenerator.GenerateNickname();
+        InsideScoring = insideScoring;
+        OutsideScoring = outsideScoring;
+        Athleticism = athleticism;
+        Playmaking = playmaking;
+        Rebounding = rebounding;
+        Defending = defending;
+        Overall = CalculateOverall();
+    }
 
-        private static readonly string[] Nicknames =
+    public static List<Player> RandomPlayer(int num)
+    {
+        Random random = new();
+        List<Player> players = new();
+        for (var i = 0; i < num; i++)
         {
-            "Ace", "Blaze", "Blitz", "Bolt", "Cypher", "Dagger", "Echo", "Ember", "Fox", "Fury", "Jinx", "Luna",
-            "Mystic", "Nyx", "Nova", "Phoenix", "Raptor", "Raven", "Rex", "Rogue", "Sage", "Sapphire", "Shadow",
-            "Serenity", "Spark", "Storm", "Typhoon", "Viper", "Vortex", "Zephyr"
-        };
-
-        public static string GenerateName()
-        {
-            Random random = new();
-            var firstNameIndex = random.Next(FirstNames.Length);
-            var lastNameIndex = random.Next(LastNames.Length);
-            var firstName = FirstNames[firstNameIndex];
-            var lastName = LastNames[lastNameIndex];
-            return firstName + " " + lastName;
+            players.Add(new Player(random.Next(70, 80), random.Next(70, 80), random.Next(70, 80),
+                random.Next(70, 80), random.Next(70, 80), random.Next(70, 80)));
         }
 
-        public static string GenerateNickname()
-        {
-            Random random = new();
-            var nicknameIndex = random.Next(Nicknames.Length);
-            var nickname = Nicknames[nicknameIndex];
-            return nickname;
-        }
+        return players;
+    }
+
+    private double CalculateOverall()
+    {
+        return (InsideScoring + OutsideScoring + Athleticism + Playmaking + Rebounding + Defending) / 6.0;
+    }
+}
+
+public static class RandomNameGenerator
+{
+    private static readonly string[] FirstNames =
+    {
+        "Aiden", "Amelia", "Aria", "Ava", "Caden", "Charlotte", "David", "Elijah", "Emily", "Grace", "Grayson",
+        "Harper", "Isabella", "Jackson", "John", "Landon", "Liam", "Lily", "Lucas", "Mateo", "Mia", "Nicole",
+        "Noah", "Olivia", "Sophia", "Sophie", "Zoe"
+    };
+
+    private static readonly string[] LastNames =
+    {
+        "Allen", "Anderson", "Brown", "Clark", "Davis", "Garcia", "Hall", "Harris", "Jackson", "Johnson", "Jones",
+        "King", "Lee", "Lewis", "Martinez", "Martin", "Miller", "Moore", "Robinson", "Rodriguez", "Smith", "Taylor",
+        "Thomas", "Thompson", "Walker", "White", "Williams", "Wilson", "Wright", "Young"
+    };
+
+    private static readonly string[] Nicknames =
+    {
+        "Ace", "Blaze", "Blitz", "Bolt", "Cypher", "Dagger", "Echo", "Ember", "Fox", "Fury", "Jinx", "Luna",
+        "Mystic", "Nyx", "Nova", "Phoenix", "Raptor", "Raven", "Rex", "Rogue", "Sage", "Sapphire", "Shadow",
+        "Serenity", "Spark", "Storm", "Typhoon", "Viper", "Vortex", "Zephyr"
+    };
+
+    public static string GenerateName()
+    {
+        Random random = new();
+        var firstNameIndex = random.Next(FirstNames.Length);
+        var lastNameIndex = random.Next(LastNames.Length);
+        var firstName = FirstNames[firstNameIndex];
+        var lastName = LastNames[lastNameIndex];
+        return firstName + " " + lastName;
+    }
+
+    public static string GenerateNickname()
+    {
+        Random random = new();
+        var nicknameIndex = random.Next(Nicknames.Length);
+        var nickname = Nicknames[nicknameIndex];
+        return nickname;
     }
 }
--- BasketLeague2.Utils/Models/Result.cs
+++ BasketLeague2.Utils/Models/Result.cs
@@ -1,51 +1,50 @@
-﻿namespace BasketLeague2.Utils.Models
+﻿namespace BasketLeague2.Utils.Models;
+
+public class Result
 {
-    public class Result
-    {
-        public DateTime Fecha { get; set; }
-        public string Equipo1 { get; set; }
-        public string Equipo2 { get; set; }
-        public int Resultado1 { get; set; }
-        public int Resultado2 { get; set; }
-    }
+    public DateTime Fecha { get; init; }
+    public string Equipo1 { get; set; } = "";
+    public string Equipo2 { get; set; } = "";
+    public int Resultado1 { get; init; }
+    public int Resultado2 { get; init; }
+}
 
-    public class AdvancedResult : Result
-    {
-        public int[][] Stats1 { get; set; }
-        public int[][] Stats2 { get; set; }
-    }
+public class AdvancedResult : Result
+{
+    public int[][] Stats1 { get; init; } = Array.Empty<int[]>();
+    public int[][] Stats2 { get; init; } = Array.Empty<int[]>();
+}
 
-    public class PlayerResult
-    {
-        public int Codigo { get; set; }
-        public int Dobles { get; set; }
-        public int Triples { get; set; }
-        public int Athleticism { get; set; }
-        public int Playmaking { get; set; }
-        public int Rebounding { get; set; }
-        public int Defending { get; set; }
-        public int Total => Dobles + Triples + Athleticism + Playmaking + Rebounding + Defending;
-    }
+public class PlayerResult
+{
+    public int Codigo { get; init; }
+    public int Dobles { get; init; }
+    public int Triples { get; init; }
+    public int Athleticism { get; init; }
+    public int Playmaking { get; init; }
+    public int Rebounding { get; init; }
+    public int Defending { get; init; }
+    public int Total => Dobles + Triples + Athleticism + Playmaking + Rebounding + Defending;
+}
 
-    public static class PlayerResultExtension
+public static class PlayerResultExtension
+{
+    /// <summary>
+    /// Converts the list of stats to a PlayerResult item
+    /// </summary>
+    /// <param name="stats">List of stats for each player</param>
+    /// <returns>The stats as a list of individual player results</returns>
+    public static IEnumerable<PlayerResult> ConvertToPlayerResult(this IEnumerable<int[]> stats)
     {
-        /// <summary>
-        /// Converts the list of stats to a PlayerResult item
-        /// </summary>
-        /// <param name="stats">List of stats for each player</param>
-        /// <returns>The stats as a list of individual player results</returns>
-        public static IEnumerable<PlayerResult> ConvertToPlayerResult(this IEnumerable<int[]> stats)
+        return stats.Select(t => new PlayerResult
         {
-            return stats.Select(t => new PlayerResult
-            {
-                Codigo = t[0],
-                Dobles = t[1],
-                Triples = t[2],
-                Athleticism = t[3],
-                Playmaking = t[4],
-                Rebounding = t[5],
-                Defending = t[6]
-            });
-        }
+            Codigo = t[0],
+            Dobles = t[1],
+            Triples = t[2],
+            Athleticism = t[3],
+            Playmaking = t[4],
+            Rebounding = t[5],
+            Defending = t[6]
+        });
     }
 }
