5e14c4f07b07e646e84559b93f0294354886229c
Ian <43787552+Ian-Inizias@users.noreply.github.com>
2024-02-20 11:15:12 +0100

    Blazorise update + Playoffs

--- BasketLeague2.App/Program.cs
+++ BasketLeague2.App/Program.cs
@@ -1,6 +1,6 @@
 using BasketLeague2.App;
 using Blazorise;
-using Blazorise.Bootstrap;
+using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,7 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
     .AddBlazorise(options => { options.Immediate = true; })
-    .AddBootstrapProviders()
+    .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
